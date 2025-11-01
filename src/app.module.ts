2962205289e533255e086ec14ec402976285a237
ravendraarief <ravendraarief@gmail.com>
2025-11-01 22:43:43 +1000

    register module preferences and social connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,22 @@ import { AuthModule } from './auth/auth.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { RegionModule } from './region/region.module';
 import { RolesModule } from './roles/roles.module';
+import { PreferencesModule } from './preferences/preferences.module';
+import { SocialConnectionModule } from './social_connection/social-connection.module';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 🔥 penting
+      envFilePath: '.env',
     }),
     SupabaseModule,
     AuthModule,
     RegionModule,
-    RolesModule
+    RolesModule,
+    PreferencesModule,
+    SocialConnectionModule
   ],
 })
 export class AppModule {}
